f524672391d8cecaa8fcf43bd7a77274e98676f0
SoarJJ <51754739+SoarJJ@users.noreply.github.com>
2020-03-29 21:58:18 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * name: Hello
+ * description: TODO
+ * date: 2020/3/29 21:54
+ *
+ * @author Soar
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,~");
+    }
+}
